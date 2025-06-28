751cf75e65d3fa7b7d5771e6976befeda7f06727
Valentine Gamma <gamma.dashboard@gmail.com>
2025-06-28 15:59:21 +0300

    Header v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Header from './Components/Header'
+import Entry from './Components/Entry'
 
 function App() {
   return (
     <>
       <Header />
+      <Entry />
     </>
   )
 }
---
+++ src/Components/Entry.jsx
@@ -0,0 +1,18 @@
+export default function Entry() {
+  return(
+    <section>
+      <article className="entry-article">
+        <img src="/src/assets/Japan.jpg" alt="Japan" className="entry-img"/>
+        <div className="entry-data">
+          <div className="fill-flex">
+            <img src="/src/assets/Fill 219.svg" alt="fill" width="7px" height="9.55px"/>
+            <p>JAPAN <span>view on google maps</span></p>
+          </div>
+          <h1>Mount Fuji</h1>
+          <p className="entry-date">12 Jan, 2023 - 24 Jan, 2023</p>
+          <p>Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+        </div>
+      </article>
+    </section>
+  )
+}
