a41ebc6c786933aac6503ee1d17cc25794170028
Valentine Gamma <gamma.dashboard@gmail.com>
2025-06-28 13:42:39 +0300

    Header

---
+++ src/Components/Header.jsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return(
+    <header>
+      
+      <nav>
+        <div className="rectangle">
+          <img src="../src/assets/globe.svg" alt="globe" />
+          <span>my travel journal</span>
+        </div>
+      </nav>
+    </header>
+  )
+}
