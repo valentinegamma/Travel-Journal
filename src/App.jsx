a2b92e5c8e4646440c3618702c563501ab164670
Valentine Gamma <gamma.dashboard@gmail.com>
2025-07-03 11:03:28 +0300

    mapping data

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import './App.css'
 import Header from './Components/Header'
-import EntryData from './Components/Entrydata'
+import Data from './Components/data'
+import Entry from './Components/Entry'
 
 function App() {
+   const EntryElement = Data.map((data) => {
+    return(
+      <Entry 
+        mainImage= {data.mainImage} 
+        alt = {data.alt}
+        country = {data.country}
+        location = {data.location}
+        date = {data.date}
+        description = {data.description}
+      />
+    )
+   })
+
   return (
     <>
       <Header />
-      <EntryData />
+      {EntryElement}
     </>
   )
 }
---
+++ src/Components/Data.jsx
@@ -0,0 +1,29 @@
+export default[
+  {
+  mainImage: "/src/assets/Japan.jpg",
+  alt: "Mount Fuji",
+  country: "Japan",
+  location: "Mount Fuji",
+  date: "12 Jan, 2023 - 24 Jan, 2023",
+  description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+}, {
+    mainImage: "/src/assets/Australia.jpg",
+    alt: "Mount Fuji",
+    country: "Australia",
+    location: "Sydney House",
+    date: "27 May, 2023 - 8 Jun, 2023",
+    description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings."
+}, {
+    mainImage: "/src/assets/Norway.jpg",
+    alt: "Geirangerfjord",
+    country: "Norway",
+    location: "Geirangerfjord",
+    date: "01 Oct, 2024 - 18 Nov, 2024",
+    description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality."
+}
+]
+     
+
+     
+
+     
